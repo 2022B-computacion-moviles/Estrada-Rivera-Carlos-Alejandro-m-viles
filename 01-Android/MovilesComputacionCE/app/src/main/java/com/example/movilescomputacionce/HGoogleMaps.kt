0b295f8321b619fbb4135fe2c01a3feff7b49b37
Gandhy García <carlos.estrada01@epn.edu.ec>
2023-02-07 12:44:53 -0500

    Maps

---
+++ 01-Android/MovilesComputacionCE/app/src/main/java/com/example/movilescomputacionce/HGoogleMaps.kt
@@ -0,0 +1,11 @@
+package com.example.movilescomputacionce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HGoogleMaps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hgoogle_maps)
+    }
+}
