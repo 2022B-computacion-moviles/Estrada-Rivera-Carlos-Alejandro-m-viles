9aa4b152edd61e1e5d04f19368113ed4d6645292
Carlos Estrada <53130383+Linkcar13@users.noreply.github.com>
2023-03-17 11:23:29 -0500

    add btn_foto_perfil

--- UltDietF/app/src/main/java/com/example/ultdietf/dashboard_activity.kt
+++ UltDietF/app/src/main/java/com/example/ultdietf/dashboard_activity.kt
@@ -1,6 +1,7 @@
 package com.example.ultdietf
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -11,7 +12,7 @@ import com.example.ultdietf.db.DbFood
 class dashboard_activity : Activity() {
     private var _bg__dashboard_ek2: View? = null
     private var image_1: ImageView? = null
-    private var vince_veras_ajiqzdaud7a_unsplash_1: ImageView? = null
+    private var btn_foto_perfil: ImageView? = null
     private var have_a_good_day_: TextView? = null
     private var my_meals: TextView? = null
     private var star: ImageView? = null
@@ -21,13 +22,14 @@ class dashboard_activity : Activity() {
     private var dinner: TextView? = null
     private var breakfast_ek1: ImageView? = null
     private var meal: ImageView? = null
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
         _bg__dashboard_ek2 = findViewById<View>(R.id._bg__dashboard_ek2) as View
         image_1 = findViewById<View>(R.id.image_1) as ImageView
-        vince_veras_ajiqzdaud7a_unsplash_1 =
-            findViewById<View>(R.id.vince_veras_ajiqzdaud7a_unsplash_1) as ImageView
+        btn_foto_perfil =
+            findViewById<View>(R.id.btn_foto_perfil) as ImageView
         have_a_good_day_ = findViewById<View>(R.id.have_a_good_day_) as TextView
         my_meals = findViewById<View>(R.id.my_meals) as TextView
         star = findViewById<View>(R.id.star) as ImageView
@@ -47,5 +49,14 @@ class dashboard_activity : Activity() {
         dinner!!.setOnClickListener {
             Toast.makeText(this, arrayListFood[1].toString(), Toast.LENGTH_LONG).show()
         }
+        btn_foto_perfil?.setOnClickListener{
+            goToActivity(perfil_activity::class.java)
+        }
+    }
+    fun goToActivity(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
